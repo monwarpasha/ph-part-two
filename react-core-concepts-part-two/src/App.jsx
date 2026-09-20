6ba71fc41a252d8ac3dab8e78b616cae26622fa1
monwarpasha <monwarpasa@gmail.com>
2026-09-20 18:34:01 +0600

    React Part Two Setup

---
+++ react-core-concepts-part-two/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+    <h1>React Apps Part Two</h1>
+
+
+    </>
+  )
+}
+
+
+
+
+
+export default App;
+
+
